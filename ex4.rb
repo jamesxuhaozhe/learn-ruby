f7cde7c0b8580affc4ea4799ac333c0dc612098d
Haozhe Xu <jamesxuhaozhe@gmail.com>
2016-08-21 21:42:47 -0700

    complete more practices

---
+++ ex4.rb
@@ -0,0 +1,16 @@
+my_name = 'Haozhe Xu'
+my_age = 27
+my_height = 100
+my_weight = 100
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} punds heavey."
+puts "Actually that's not too heavey."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teetch are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
